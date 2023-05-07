55f41b23c56a0915c817ea2d680ba3b94ba7dd0a
timtonix <timtonic@outlook.fr>
2023-05-07 12:42:49 +0200

    the sprite move

--- main.py
+++ main.py
@@ -4,26 +4,54 @@ import numpy
 import time
 import random
 
+
 class App:
     def __init__(self):
-        pyxel.init(128, 128, "Street_Pyxel", 60, pyxel.KEY_ESCAPE)
+        pyxel.init(128, 128, "Street_Pyxel", 30, pyxel.KEY_ESCAPE)
+        pyxel.load("sample.pyxres")
+        self.fighter_left = Fighter("left")
+        self.fighter_right = Fighter("right")
+
+        pyxel.run(self.update, self.draw)
+
+    def draw(self):
+        pyxel.cls(0)
+        self.fighter_left.draw()
+        self.fighter_right.draw()
+
+    def update(self):
+        if self.fighter_left.collision(self.fighter_right.x):
+            if pyxel.btn(pyxel.KEY_D):
+                self.fighter_left.x += 1
+            if pyxel.btn(pyxel.KEY_Q):
+                self.fighter_left.x -= 1
+            if pyxel.btn(pyxel.KEY_Z):
+                pass
+        if pyxel.btn(pyxel.KEY_LEFT):
+            self.fighter_right.x -= 1
+        if pyxel.btn(pyxel.KEY_RIGHT):
+            self.fighter_right.x += 1
 
 
 class Fighter:
-    def __init__(self):
-        pass
 
-    def update(self):
-        if pyxel.btnp(pyxel.KEY_D):
-            self.move('right')
-        if pyxel.btnp(pyxel.KEY_Q):
-            self.move('left')
-        if pyxel.btnp(pyxel.KEY_Z):
-            self.move('up')
+    def __init__(self, pos):
+        if pos == "left":
+            self.x = 30
+        elif pos == "right":
+            self.x = 100
+        else:
+            print("only accept left or right")
 
+        self.y = 100
     def draw(self):
-        pass
+        pyxel.blt(self.x, self.y, 0, 128, 16, 16, 16, 0)
 
-    def move(self, direction):
-        pass
+    def collision(self, x):
+        if self.x < x or self.x > x + 16:
+            return False
+
+
+
+
 App()
